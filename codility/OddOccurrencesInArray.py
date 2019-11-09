86273b1b3f3d344c747f3b17841d75788f8d76a4
kh-mo <momkdh2@naver.com>
2019-11-10 01:14:52 +0900

    solve problem!

--- codility/OddOccurrencesInArray.py
+++ codility/OddOccurrencesInArray.py
@@ -12,6 +12,7 @@ A[0] = 9  A[1] = 3  A[2] = 9 A[3] = 3  A[4] = 9  A[5] = 7 A[6] = 9
 
 배열 크기는 홀수 정수이며 [1..1,000,000]안에 있다.
 배열의 요소는 [1..1,000,000,000] 범위 내에 있다.
+A 배열 중 한 요소를 제외한 다른 숫자는 짝수번 발생한다 => 한 숫자는 홀수번 반복한다는 의미다(line 29가 문제였다)
 '''
 
 from collections import Counter
@@ -25,13 +26,5 @@ def solution(A):
         dic[i] += 1
 
     for key, value in dic.items():
-        if value == 1:
+        if value%2 == 1:
             return key
-
-A = [1,1,1,1,2]
-solution(A)
-
-'''
-Invalid result type, int expected, <class 'NoneType'> found.
-이거슨 대체 왜 발생하는 문제인가...
-'''
